9b80f2b5a444f8cb83e2fa6a128401ff122787fc
Launchpad <Launchpad@192.168.0.151>
2021-05-18 19:21:38 -0300

    New commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6d79c7d6-dd12-40d3-b6ed-a91cee06e09a</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1d7840c-944c-40ea-9c21-51d9bbe453d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1_git</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>15229c10-884c-4448-a0ab-af1375cea66c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2_git</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
